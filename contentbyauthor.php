1b73d14e5b17037a207097cfc704d3e90a10458a
webmaster-cepr <webmaster@cepr.net>
2013-10-07 16:30:28 -0400

    Working version

---
+++ contentbyauthor.php
@@ -0,0 +1,63 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+ 
+class plgContentContentByAuthor extends JPlugin {
+
+
+        public function onContentPrepare($context, &$row, &$params ) {
+		
+		$db = JFactory::getDBO();
+        
+		if (preg_match_all("#{author}(.*?){/author}#s", $row->text, $matches, PREG_PATTERN_ORDER) > 0) {
+		
+			foreach( $matches[0] as $match ){
+			
+				$_temp = preg_replace("/{.+?}/", "", $match);
+				unset($_params);
+				$_params = explode(":",$_temp);
+				$_author = $_params[0];
+				$_year = $_params[1];
+				$_cat = html_entity_decode($_params[2]);
+				$_limit = $_params[3];
+				$i = 0;
+				$now = date('Y-m-d H:i:s');
+				
+ 				$sql = "SELECT id, title, alias, introtext AS intro, catid, DATE_FORMAT(created, '%M %d, %Y') AS created, created AS sortdate "
+				. "\n FROM #__content "
+				. "\n WHERE state = 1 ";
+				
+				if( $_author ) {
+					$sql .= "\n AND created_by_alias LIKE '%" . $_author . "%' ";
+				}
+				if( $_year ){
+					$sql .= "\n AND created BETWEEN '" . $_year . "-01-01' AND '" . $_year . "-12-31' ";
+				}
+				if( $_cat ){
+					$sql .= "\n AND catid = " . $_cat . " ";
+				}				
+
+				$sql .= "\n ORDER BY sortdate DESC";
+
+				$db->setQuery($sql);				
+				$items = $db->loadObjectList();
+
+				$html = '';
+
+				foreach($items as $item ){
+					$html .= '<p><a href="'.$this->basurl.'/index.php?option=com_content&id='.$item->id.'&view=article">'.$item->title.'</a><br />'
+					."\n".$item->intro.'</p>';
+					$i++;
+					
+					if ($i == $_limit) { break; }
+					
+				}
+				
+				$row->text = preg_replace( "#{author}".$_temp."{/author}#s", $html , $row->text );				
+			
+			
+			}
+		
+		}
+	}
+}	
